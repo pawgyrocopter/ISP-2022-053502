56d4cdb1940569d857261a64d2068fae68851e95
savely.volodkov@gmail.com <savely.volodkov@gmail.com>
2022-05-11 14:41:05 +0300

    setup.py file created

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+
+setup(name='Gyrocopter',
+      version='1.0',
+      description='Objects serializer library',
+      author='Volodkov Savely',
+      packages=['gyrocopter'])
